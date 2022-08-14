19194b651a9ac23d6acd1575ef7ab7519bbe6a49
hendrickdias66 <hendrickdias66@gmail.com>
2022-08-14 14:04:11 +0100

    Line score rendering

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,31 @@ fn can_move(piece: u8, orientation: usize, pos_x: i32, pos_y: i32, player_field:
     true
 }
 
+fn render_line(lines: &mut Vec<usize>, buffer: &String, player_field: &mut [usize]) -> () {
+    // Draw line completion
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::White),
+        SetBackgroundColor(Color::DarkBlue),
+        Print(buffer),
+        SetSize(SCREEN_WIDTH as u16, SCREEN_HEIGTH as u16)
+    )
+    .unwrap();
+    std::thread::sleep(Duration::from_millis(60)); // Delay a bit
+
+    for l in lines {
+        for px in 0..FIELD_WIDTH - 1 {
+            for py in *l as i32..0 {
+                player_field[py as usize * FIELD_WIDTH + px] =
+                    player_field[(py as usize - 1) * FIELD_WIDTH + px];
+            }
+            player_field[px] = 0;
+        }
+    }
+
+    print!("{esc}c", esc = 27 as char); //Clear terminal windows
+}
+
 fn main() {
     let mut player_field: Vec<usize> = vec![0usize; FIELD_WIDTH * FIELD_HEIGTH]; // Create play field buffer
 
@@ -80,14 +105,16 @@ fn main() {
     let mut pos_y: i32 = 0;
     let mut orientation = 0;
     let mut piece: u8 = rand::thread_rng().gen_range(0..7);
+    // let mut piece: u8 = 2; // For debugging
     let mut is_falling: bool = false;
-    let game_over = false;
+    let mut game_over = false;
     let speed = 20;
     let mut speed_count = 0;
+    let mut lines: Vec<usize> = Vec::new();
 
     while !game_over {
         // Timing =======================
-        std::thread::sleep(Duration::from_millis(50)); // Small Step = 1 Game Tick
+        std::thread::sleep(Duration::from_millis(10)); // Small Step = 1 Game Tick
         speed_count += 1;
         is_falling = speed_count == speed;
 
@@ -95,7 +122,7 @@ fn main() {
         let mut screen: Vec<char> = vec![' '; SCREEN_WIDTH * SCREEN_HEIGTH];
 
         // Game Logic =====================
-        if poll(Duration::from_millis(5000)).unwrap() {
+        if poll(Duration::from_millis(10)).unwrap() {
             // It's guaranteed that the `read()` won't block when the `poll()`
             // function returns `true`
             match read().unwrap() {
@@ -112,7 +139,7 @@ fn main() {
                         pos_y +=
                             can_move(piece, orientation, pos_x, pos_y + 1, &player_field) as i32
                     }
-                    KeyCode::Char('w') => {
+                    KeyCode::Char('r') => {
                         orientation +=
                             can_move(piece, orientation + 1, pos_x, pos_y, &player_field) as usize
                     }
@@ -146,10 +173,29 @@ fn main() {
                     }
                 }
 
+                // Check for lines
+                for py in 0..4 as usize {
+                    if pos_y as usize + py < FIELD_HEIGTH - 1 {
+                        let mut line = true;
+                        for px in 1..FIELD_WIDTH - 1 {
+                            line &= player_field[(pos_y as usize + py) * FIELD_WIDTH + px] != 0;
+                        }
+
+                        if line {
+                            // Remove Line, set to =
+                            for px in 0..FIELD_WIDTH - 1 {
+                                player_field[(pos_y as usize + py) * FIELD_WIDTH + px] = 8;
+                            }
+                            lines.push(pos_y as usize + py);
+                        }
+                    }
+                }
+
                 pos_x = FIELD_WIDTH as i32 / 2;
                 pos_y = 0;
                 orientation = 0;
                 piece = rand::thread_rng().gen_range(0..7);
+                // piece = 2; // For debbuging
             }
         }
 
@@ -181,13 +227,22 @@ fn main() {
         }
 
         let buffer: String = screen.into_iter().collect();
+
+        // Animate Line Completion
+        if !lines.is_empty() {
+            render_line(&mut lines, &buffer, &mut player_field);
+            lines.clear();
+        }
         execute!(
             stdout(),
             SetForegroundColor(Color::White),
             SetBackgroundColor(Color::DarkBlue),
-            Print(buffer),
+            Print(buffer.clone()),
             SetSize(SCREEN_WIDTH as u16, SCREEN_HEIGTH as u16)
         )
         .unwrap();
+
+        // Lose state
+        game_over = !can_move(piece, orientation, pos_x, pos_y, &player_field);
     }
 }
